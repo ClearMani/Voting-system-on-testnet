3a904a326acf3135326085253a486625db8ec589
ClearMani <doli.manichandra@cleartax.in>
2022-08-23 18:51:46 +0530

    Minimum viable voting system

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sample Hardhat Project
+
+This project is a initial version of voting system contract along which can be readily deployed on goerli test network by adding the RPC URL (containing api key) and a private key of any account which contains goerli eth.
+
+to get test goerli eth you can use any of the following faucets by connecting to a wallet
+
+1. https://goerlifaucet.com/
+2. https://faucets.chain.link/goerli
---
+++ scripts/deploy.ts
@@ -0,0 +1,43 @@
+import hre, { ethers, run, network } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log("Deploying contract...");
+  const Token = await ethers.getContractFactory("Ballot");
+  const token = await Token.deploy();
+  await token.deployed();
+
+  if (process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for 6 block confirmations........");
+    await token.deployTransaction.wait(6);
+    console.log("6 blocks confirmed");
+    await verify(token.address, []);
+  }
+
+  console.log("Token address:", token.address);
+}
+
+const verify = async (contractAddress: any, args: any) => {
+  console.log("Verifying deployed contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArgs: args,
+    });
+  } catch (e) {
+    if ((e as any)?.message.toLowerCase().includes("already verified")) {
+      console.log("contract is already deployed and verified");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
